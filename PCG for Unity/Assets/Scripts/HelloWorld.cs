98d5aa58d620caa132d967c3526440ca598a9a1b
Rudengs <rudengkim@gmail.com>
2019-01-01 13:28:55 +0900

    Pseudo Random Numbers

---
+++ PCG for Unity/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour {
+
+    public string[] hellos = new string[4]
+    {
+        "Hello World", "Hola Mundo", "Bonjour Le Monde", "Hallo Welt"
+    };
+
+    public Text textString;
+
+	void Start () 
+    {
+        Random.seed = (int)System.DateTime.Now.Ticks;
+        int randomIndex = Random.Range(0, 4);
+        textString.text = hellos[randomIndex];
+	}
+}
